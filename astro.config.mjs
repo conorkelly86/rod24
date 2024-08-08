5813d01eea274d38a2796c465dadcbf053b2782e
conorkelly86 <conorkelly86@gmail.com>
2024-08-08 10:22:31 +0100

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://conorkelly86.github.io/rod24/",
-  base: "/RecipeAstroApi",
+  //   base: "/rod24",
 });
